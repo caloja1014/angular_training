0bb636541dd3461133a47ab05b15df9f580b960c
Carlos Loja <carlos.loja@thebridge.com>
2025-10-20 10:59:50 -0500

    Adding product page

---
+++ src/app/pages/product-view/product-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-view',
+  imports: [],
+  templateUrl: './product-view.component.html',
+  styleUrl: './product-view.component.scss'
+})
+export class ProductViewComponent {
+
+}
