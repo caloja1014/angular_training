dc35ae7157b46b78a10cfcd55c5d229e7953590e
Carlos Loja <carlos.loja@thebridge.com>
2025-10-20 12:02:12 -0500

    Adding routes for prodcut view

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, input } from '@angular/core';
 import { Product } from '../../models/products.model';
 import { PrimaryButtonComponent } from "../primary-button/primary-button.component";
 import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
@@ -12,4 +13,8 @@ import { CartService } from '../../services/cart.service';
 export class ProductCardComponent {
   product = input.required<Product>();
   cartService = inject(CartService);
+  private router = inject(Router)
+  viewProduct() {
+    this.router.navigate(['/product', this.product().id]);
+  }
 }
